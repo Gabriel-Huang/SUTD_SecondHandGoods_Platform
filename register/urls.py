1ff1cc61cf8113e1f20d4c4a93c64a0a28565a1e
GilbertZhang <jiaxuan_zhang@mymail.sutd.edu.sg>
2017-11-23 11:42:25 +0800

    add carousel in the homepage

--- register/urls.py
+++ register/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'register'
 urlpatterns = [
     url(r'^register/$', views.register, name='register'),
-    url(r'^signin/$', views.signin, name='login'),
+    url(r'^login/$', views.signin, name='login'),
     url(r'^signout/$', views.signout, name='logout'),
     url(r'^profile/$', views.profile, name='profile'),
 ]
