024ed2ef67c3362caa162baf06b823ff8583c3a7
GilbertZhang <zjx1997@163.com>
2017-11-25 02:50:56 +0800

    add feedback and rating in feedback page

--- core/settings.py
+++ core/settings.py
@@ -76,26 +76,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'platform',
-        'USER': 'jiahui',
-        'PASSWORD': 'Wehjhtc123.',
-        'HOST':'',
-        'PORT':'',
-    }
-}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'platform',
-#         'USER': 'gilbert',
-#         'PASSWORD': '',
+#         'USER': 'jiahui',
+#         'PASSWORD': 'Wehjhtc123.',
 #         'HOST':'',
 #         'PORT':'',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'platform',
+        'USER': 'gilbert',
+        'PASSWORD': '',
+        'HOST':'',
+        'PORT':'',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- register/forms.py
+++ register/forms.py
@@ -5,6 +5,7 @@ from django import forms
 #     email = forms.EmailField(required = True)
 #     password = forms.CharField(required = False, max_length = 100)
 
+
 class signinForm(forms.Form):
-    username  = forms.CharField(required = False, max_length = 100)
-    password = forms.CharField(widget = forms.PasswordInput)
+    username = forms.CharField(required=False, max_length = 100)
+    password = forms.CharField(widget=forms.PasswordInput)
