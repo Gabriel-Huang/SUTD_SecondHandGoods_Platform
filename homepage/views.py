066e5292ec812c7d92463a4bd57332334bf33cdf
GilbertZhang <zjx1997@163.com>
2017-11-27 02:06:30 +0800

    redirect homepage url

--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,47 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
 from django.shortcuts import render
+from django.db import connection
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def home(request):
-    context = locals()
     template = 'home.html'
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT username FROM auth_user")
+        popular_seller = dictfetchall(cursor)
+    for user in popular_seller:
+        user['get_absolute_url'] = reverse('user-view', args=[str(user['username'])])
+    context = {"popular_seller": popular_seller}
     return render(request, template, context)
+
+
+@login_required
+def user_view(request, pk):
+    template = 'profile_other.html'
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT p_name FROM Product WHERE sellerid = %s", [pk])
+        row = cursor.fetchall()
+        cursor.execute("SELECT FeedbackUser, f_content, f_date, Product, f_id, p_name "
+                       "FROM Feedback, Product "
+                       "WHERE Seller = %s "
+                       "AND Feedback.Product = Product.p_id",
+                       [pk])
+        comment_list = dictfetchall(cursor)
+        for comment in comment_list:
+            comment['date_ago'] = (datetime.datetime.now().date() - comment['f_date']).days
+    context = {'product_list': row,
+               'comment_list': comment_list}
+    return render(request, template, context)
+
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
