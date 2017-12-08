7b6d0b1a4b66ee4b0d1349d12b4b70b09b61bfdb
CreoleLady <creole12138@gmail.com>
2017-12-08 20:32:46 +0800

    alias added

---
+++ statistics.sql
@@ -0,0 +1,26 @@
+-- Popular seller
+
+SELECT productseller FROM (
+SELECT count(o_id), productseller FROM OrderRecord
+GROUP BY productseller
+ORDER BY count(o_id) DESC
+LIMIT 5) AS COUNT;
+
+-- Popular items
+
+SELECT * FROM Product
+Where p_id IN
+(SELECT productid FROM (
+SELECT productid, count(o_id) FROM OrderRecord
+WHERE trade_result = 0
+GROUP BY productid
+ORDER BY count(o_id) DESC
+LIMIT 5)AS COUNT);
+
+-- Average rating for given comment
+
+SELECT Feedback.f_content, score FROM(
+SELECT Feedback.fid, Feedback.f_content, AVG(Rating.r_score) AS score FROM Feedback, Rating, auth_user
+WHERE Feedback.f_id = Rating.Feedbackid 
+AND Feedback.user = '%s' %(username)
+GROUP BY Feedback.f_id) AS F;
